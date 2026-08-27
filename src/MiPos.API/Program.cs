82222db77591da7ffe33019386916d0c18ffcaaf
Manuelfg1985 <manuelfg2@gmail.com>
2026-08-27 10:53:25 -0300

    Fix: escuchar en 0.0.0.0 con puerto dinamico de Render

--- src/MiPos.API/Program.cs
+++ src/MiPos.API/Program.cs
@@ -3,6 +3,9 @@ using MiPos.API.Hubs;
 using MiPos.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 MercadoPagoConfig.AccessToken = builder.Configuration["MercadoPago:AccessToken"];
 
 string accessToken = builder.Configuration["MercadoPago:AccessToken"] 
