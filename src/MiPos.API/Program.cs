9ac1468e9539b1dba6928213e6404b74e03ceda3
Manuelfg1985 <manuelfg2@gmail.com>
2026-08-27 11:07:50 -0300

    Fix: permitir todos los hosts (AllowedHosts) para Render

--- src/MiPos.API/Program.cs
+++ src/MiPos.API/Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+builder.Services.Configure<Microsoft.AspNetCore.HostFiltering.HostFilteringOptions>(options =>
+{
+    options.AllowedHosts = new[] { "*" };
+});
 MercadoPagoConfig.AccessToken = builder.Configuration["MercadoPago:AccessToken"];
 
 string accessToken = builder.Configuration["MercadoPago:AccessToken"] 
